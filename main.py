76ccbc80e0f296743fe311fbf8970ff15f28c724
Chinwendu <chinwenduukoha@gmail.com>
2022-05-02 10:26:58 -0400

    1-3 done

---
+++ main.py
@@ -0,0 +1,9 @@
+students = ['Jenna', 'Molly', 'Nghiem', 'Chinwendu']
+print(students[1])
+print(students[3])
+
+foods = ('Fried Rice', 'Jollof Rice', 'Efo-riro', 'Nkwobi')
+for word in foods:
+    print(word + ' is a good food')
+for word in foods:
+    print(foods[2:4])
